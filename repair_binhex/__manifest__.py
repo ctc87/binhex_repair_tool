e9f48569b0e450218f5f0482249bdb2c403a03c6
ctc87 <carlos.troyano.carmona@gmail.com>
2019-10-17 13:33:46 +0200

    put end values

--- repair_binhex/__manifest__.py
+++ repair_binhex/__manifest__.py
@@ -2,6 +2,8 @@
     'name': 'Repair modification for stock operations',
     'category': 'Add stock operations in repair tool in Odoo to repair external products.',
     'summary': 'Add stock operations in repair tool in Odoo to repair external products.',
+    'price': 30,
+    'currency': 'EUR',
     'version': '1.0.0',
     'license': 'AGPL-3',
     'author':  'Binhex Sysems',
